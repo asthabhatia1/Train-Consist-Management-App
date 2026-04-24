a990ec3f5cf951d8c5964db043d45b1bf6b5f6bd
asthabhatia1 <astha18bhatia@gmail.com>
2026-04-24 09:52:25 +0530

    UC17

--- src/TrainConsistManagement.java
+++ src/TrainConsistManagement.java
@@ -1,71 +1,39 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class TrainConsistManagementApp {
-
-
-    public static void bubbleSort(int[] capacities) {
-        int n = capacities.length;
-
-
-        for (int i = 0; i < n - 1; i++) {
-
-
-            for (int j = 0; j < n - i - 1; j++) {
-
-
-                if (capacities[j] > capacities[j + 1]) {
-
-
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-
-    public static void printArray(int[] arr) {
-        for (int val : arr) {
-            System.out.print(val + " ");
-        }
-        System.out.println();
-    }
-
+public class TrainConsistManagement {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== Train Consist Management App (UC16 - Bubble Sort) ===");
+        System.out.println("=== Train Consist Management App (UC17 - Arrays.sort) ===");
 
         try {
 
-            System.out.print("Enter number of passenger bogies: ");
+            System.out.print("Enter number of bogies: ");
             int n = Integer.parseInt(scanner.nextLine());
 
-            int[] capacities = new int[n];
+            String[] bogieNames = new String[n];
 
 
-            System.out.println("Enter capacities:");
+            System.out.println("Enter bogie names:");
             for (int i = 0; i < n; i++) {
-                capacities[i] = Integer.parseInt(scanner.nextLine());
+                bogieNames[i] = scanner.nextLine();
             }
 
 
-            System.out.print("\nOriginal capacities: ");
-            printArray(capacities);
-
-            bubbleSort(capacities);
+            System.out.println("\nOriginal bogie names: " + Arrays.toString(bogieNames));
 
 
-            System.out.print("Sorted capacities: ");
-            printArray(capacities);
+            Arrays.sort(bogieNames);
+
+            System.out.println("Sorted bogie names: " + Arrays.toString(bogieNames));
 
         } catch (NumberFormatException e) {
             System.out.println("Invalid input! Please enter numeric values only.");
         } finally {
             scanner.close();
-            System.out.println("\n🚆 Sorting operation completed.");
+            System.out.println("\n🚆 Sorting completed using Arrays.sort().");
         }
     }
 }
